881903e7327c79b4b02094f99492e3e3f2ba9533
Emory Merryman <emorymerryman@Emorys-MacBook-Pro.local>
2016-05-20 11:49:15 -0400

    fixed su

--- init.sh
+++ init.sh
@@ -6,5 +6,5 @@ chmod 0444 /etc/sudoers.d/ &&
 mkdir /home/${LUSER}/.ssh &&
 cp /opt/dreadfultiger/private/id_rsa /home/${LUSER}/.ssh &&
 chown --recursive ${LUSER}:${LUSER} /home/${LUSER}/.ssh &&
-su --login ${LUSER}  "/opt/dreadfultiger/bin/setup \"${GIT_EMAIL}\" \"${GIT_NAME}\" \"${GIT_URL}\" \"${GIT_BRANCH_PARENT}\" \"${GIT_BRANCH_CHILD}\""
+su --login ${LUSER} /opt/dreadfultiger/bin/setup "${GIT_EMAIL}" "${GIT_NAME}" "${GIT_URL}" "${GIT_BRANCH_PARENT}" "${GIT_BRANCH_CHILD}"
 su --login ${LUSER}

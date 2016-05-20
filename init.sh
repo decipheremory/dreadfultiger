8f9bdf684ca1276f1ff83b5a7c7764d70e45ec34
Emory Merryman <emorymerryman@Emorys-MacBook-Pro.local>
2016-05-20 11:45:32 -0400

    corrected error with env

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-adduser --create-home  &&
-echo "  ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/ &&
+adduser --create-home ${LUSER} &&
+echo "${LUSER} ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/${LUSER} &&
 chmod 0444 /etc/sudoers.d/ &&
-mkdir /home//.ssh &&
-cp /opt/dreadfultiger/private/id_rsa /home//.ssh &&
-chown --recursive : /home//.ssh &&
-su --login  --command "/opt/dreadfultiger/bin/setup \"\" \"\" \"\" \"\" \"\""
-su --login 
+mkdir /home/${LUSER}/.ssh &&
+cp /opt/dreadfultiger/private/id_rsa /home/${LUSER}/.ssh &&
+chown --recursive ${LUSER}:${LUSER} /home/${LUSER}/.ssh &&
+su --login ${LUSER}  "/opt/dreadfultiger/bin/setup \"${GIT_EMAIL}\" \"${GIT_NAME}\" \"${GIT_URL}\" \"${GIT_BRANCH_PARENT}\" \"${GIT_BRANCH_CHILD}\""
+su --login ${LUSER}
--- setup.sh
+++ setup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-GIT_EMAIL="" &&
-GIT_NAME="" &&
-GIT_URL="" &&
-GIT_BRANCH_PARENT="" &&
-GIT_BRANCH_CHILD="" &&
-cp /opt/dreadfultiger/lib/config /home/emory/.ssh &&
-chmod 0600 /home/emory/.ssh/config /home/emory/.ssh/id_rsa &&
-git config --global user.email "" &&
-git config --global user.name "" &&
-mkdir /home/emory/workspace &&
-git -C /home/emory/workspace init &&
-git -C /home/emory/workspace remote add origin "" &&
+GIT_EMAIL="${1}" &&
+GIT_NAME="${2}" &&
+GIT_URL="${3}" &&
+GIT_BRANCH_PARENT="${4}" &&
+GIT_BRANCH_CHILD="${5}" &&
+cp /opt/dreadfultiger/lib/config ${HOME}/.ssh &&
+chmod 0600 /home/emory/.ssh/config ${HOME}/.ssh/id_rsa &&
+git config --global user.email "${GIT_EMAIL}" &&
+git config --global user.name "${GIT_NAME}" &&
+mkdir ${HOME}/workspace &&
+git -C ${HOME}/workspace init &&
+git -C /home/emory/workspace remote add origin "${GIT_URL}" &&
 # ([ -z "" ] || git -C /home/emory/workspace pull origin "") &&
 # ([ -z "" ] || git -C /home/emory/workspace checkout -b "") &&
 true
